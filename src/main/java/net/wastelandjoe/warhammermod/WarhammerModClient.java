5fd30ae8470cfc0ff36d9b72a0eaa3765619db1f
Joshua Eckman <joshuaeckman518@gmail.com>
2024-01-11 13:36:41 -0500

    Initial commit

---
+++ src/main/java/net/wastelandjoe/warhammermod/WarhammerModClient.java
@@ -0,0 +1,10 @@
+package net.wastelandjoe.warhammermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WarhammerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wastelandjoe/warhammermod/WarhammerModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wastelandjoe.warhammermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WarhammerModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
